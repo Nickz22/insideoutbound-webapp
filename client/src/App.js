53ffe0b5b860d9deeedf56f4a9fc021517539db3
nzozaya <nzozaya@sitetracker.com>
2024-06-10 17:58:29 -0500

    integrate client and server

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
+        const response = await fetch(`${apiBaseUrl}`);
+        const data = await response.json();
+        setMessage(data.message);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>{message}</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
