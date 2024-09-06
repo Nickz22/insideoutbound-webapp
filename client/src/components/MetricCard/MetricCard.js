70d6aba817603a625b957553f5f7085f4a857d92
ter1203 <terry.hopper1203@gmail.com>
2024-09-06 09:22:46 -0300

    feat: step 2, 3, 4

--- client/src/components/MetricCard/MetricCard.js
+++ client/src/components/MetricCard/MetricCard.js
@@ -1,27 +1,25 @@
 import React from "react";
-import { Box, Tooltip, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
-const MetricCard = ({ title, value, subText, tooltipTitle }) => {
+const MetricCard = ({ title, value, subText }) => {
   return (
-    <Tooltip title={tooltipTitle || ''} arrow>
-      <Box
-        sx={{
-          border: "1px solid #ccc",
-          borderRadius: "8px",
-          padding: "16px",
-          textAlign: "center",
-          minWidth: "150px",
-          margin: "16px",
-          boxShadow: "0px 2px 4px rgba(0,0,0,0.1)",
-        }}
-      >
-        <Typography variant="h6">{title}</Typography>
-        <Typography variant="h4" sx={{ color: "teal", margin: "8px 0" }}>
-          {value}
-        </Typography>
-        <Typography variant="body2">{subText}</Typography>
-      </Box>
-    </Tooltip>
+    <Box
+      sx={{
+        border: "1px solid #ccc",
+        borderRadius: "8px",
+        padding: "16px",
+        textAlign: "center",
+        minWidth: "150px",
+        margin: "16px",
+        boxShadow: "0px 2px 4px rgba(0,0,0,0.1)",
+      }}
+    >
+      <Typography variant="h6">{title}</Typography>
+      <Typography variant="h4" sx={{ color: "teal", margin: "8px 0" }}>
+        {value}
+      </Typography>
+      <Typography variant="body2">{subText}</Typography>
+    </Box>
   );
 };
 
