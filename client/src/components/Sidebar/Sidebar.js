06c2397e4a28e4050f4b2348dd341157b328320a
ter1203 <terry.hopper1203@gmail.com>
2024-09-10 17:42:13 -0300

    feat: sidebar new style

--- client/src/components/Sidebar/Sidebar.js
+++ client/src/components/Sidebar/Sidebar.js
@@ -12,6 +12,10 @@ import {
 } from "@mui/material";
 import { logout, getLoggedInUser } from "../Api/Api";
 import "./Sidebar.css";
+import Logo from "../Logo/Logo";
+import ProspectIcon from "../icons/ProspectIcon";
+import SettingIcon from "../icons/SettingIcon";
+import TaskQueryCounterIcon from "../icons/TaskQueryCounterIcon";
 
 const CustomNavLink = ({ to, label, icon, alignIconRight = false }) => {
   const location = useLocation();
@@ -24,24 +28,34 @@ const CustomNavLink = ({ to, label, icon, alignIconRight = false }) => {
       className={isActive ? "active" : ""}
       sx={{
         "&:hover": {
-          color: "blue",
-          fontWeight: "bold",
+          fontWeight: "500",
+          color: "#ffffff",
+          background: "linear-gradient(168deg, #ff7d2f 24.98%, #491eff 97.93%)"
         },
+        "&:hover > *": {
+          color: "#ffffff",
+        },
+        fontSize: "18px",
         display: "flex",
-        justifyContent: "space-between",
+        justifyContent: "flex-start",
+        gap: "18px",
+        fontWeight: isActive ? "500" : "300",
+        color: isActive ? "#ffffff" : "#879FCA",
+        background: isActive ? "linear-gradient(168deg, #ff7d2f 24.98%, #491eff 97.93%)" : "transparent",
       }}
     >
-      <ListItemText primary={label} />
       {icon && (
         <ListItemIcon
           sx={{
             minWidth: "auto",
             marginLeft: alignIconRight ? "auto" : 0,
+            color: isActive ? "#ffffff" : "#879FCA",
           }}
         >
           {icon}
         </ListItemIcon>
       )}
+      <ListItemText primary={label} />
     </ListItemButton>
   );
 };
@@ -79,23 +93,30 @@ const Sidebar = () => {
       variant="permanent"
       sx={{
         width: 240,
+        backgroundColor: "rgba(30, 36, 47, 1)",
         flexShrink: 0,
         [`& .MuiDrawer-paper`]: {
           width: 240,
           boxSizing: "border-box",
           display: "flex",
           flexDirection: "column",
+          backgroundColor: "rgba(30, 36, 47, 1)",
+          paddingTop: "47px"
         },
       }}
     >
+      <div style={{ display: "flex", flexDirection: "row", justifyContent: "center", width: "100%", marginBottom: "36px" }}>
+        <Logo />
+      </div>
+
+      <Divider sx={{ backgroundColor: "rgba(135, 159, 202, 0.5)", margin: "0px 33px 28px" }} />
       <List>
-        <CustomNavLink to="/app/prospecting" label="Prospecting" />
-        <Divider />
-        <CustomNavLink to="/app/settings" label="Settings" />
-        <Divider />
+        <CustomNavLink to="/app/prospecting" label="Prospecting" icon={<ProspectIcon />} />
+        <CustomNavLink to="/app/settings" label="Settings" icon={<SettingIcon />} />
         <CustomNavLink
           to="/app/task-query-counter"
           label="Task Query Counter"
+          icon={<TaskQueryCounterIcon />}
         />
       </List>
       <Box sx={{ marginTop: "auto", marginBottom: 2 }}>
@@ -105,8 +126,19 @@ const Sidebar = () => {
           icon={<Avatar src={userPhoto} sx={{ width: 24, height: 24 }} />}
           alignIconRight={true}
         />
-        <Divider />
-        <ListItemButton onClick={handleLogout}>
+        <ListItemButton
+          sx={{
+            "&:hover": {
+              fontWeight: "500",
+              color: "#ffffff",
+              background: "linear-gradient(168deg, #ff7d2f 24.98%, #491eff 97.93%)"
+            },
+            color: "#879FCA",
+            fontSize: "18px",
+            fontWeight: "300",
+          }}
+          onClick={handleLogout}
+        >
           <ListItemText primary="Logout" />
         </ListItemButton>
       </Box>
