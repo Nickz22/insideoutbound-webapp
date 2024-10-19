c320c20511de8a6f7dce96e2b1d9b8759609551c
ter1203 <terry.hopper1203@gmail.com>
2024-10-19 13:28:18 -0300

    feat: prospecting metadata chart

--- client/src/components/ProspectingActiveAccount/SelectedAccount.js
+++ client/src/components/ProspectingActiveAccount/SelectedAccount.js
@@ -3,24 +3,6 @@ import { Box, Card, Grid } from "@mui/material";
 import SummaryBarChartCard from "../../components/SummaryCard/SummaryBarChartCard";
 
 function SelectedAccount({ selectedActivation }) {
-  const mockData = [
-    {
-      label: "User 1",
-      value: 6,
-    },
-    {
-      label: "User 2",
-      value: 4,
-    },
-    {
-      label: "User 3",
-      value: 7,
-    },
-    {
-      label: "User 4",
-      value: 2,
-    },
-  ];
   return (
     <Box sx={{ mt: 4 }}>
       <Card
@@ -41,7 +23,7 @@ function SelectedAccount({ selectedActivation }) {
         </Box>
       </Card>
       <Grid container spacing={2}>
-        <Grid item xs={6}>
+        <Grid item xs={12}>
           <Card
             sx={{
               borderRadius: "20px",
@@ -55,33 +37,12 @@ function SelectedAccount({ selectedActivation }) {
             <Box sx={{ overflowX: "auto", width: "100%" }}>
               <Box sx={{ minWidth: "200px", height: "250px" }}>
                 <SummaryBarChartCard
-                  direction="vertical"
-                  title="Effort"
-                  target={5}
-                  data={mockData}
-                />
-              </Box>
-            </Box>
-          </Card>
-        </Grid>
-        <Grid item xs={6}>
-          <Card
-            sx={{
-              borderRadius: "20px",
-              boxShadow: "0px 0px 25px rgba(0, 0, 0, 0.1)",
-              paddingX: 4,
-              paddingTop: 4,
-              marginX: "auto",
-              marginBottom: 4,
-            }}
-          >
-            <Box sx={{ overflowX: "auto", width: "100%" }}>
-              <Box sx={{ minWidth: "200px", height: "250px" }}>
-                <SummaryBarChartCard
-                  direction="vertical"
-                  title="Outcomes"
-                  target={5}
-                  data={mockData}
+                  direction="horizontal"
+                  title="Prospecting Metadata"
+                  data={selectedActivation.prospecting_metadata.map((item) => ({
+                    label: item.name,
+                    value: item.total,
+                  }))}
                 />
               </Box>
             </Box>
